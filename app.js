e8420d31bfaaac71e8d5d695f046183dcba2bb8e
cwstw <cwstw@naver.com>
2023-07-27 22:12:17 +0900

    2023-07-27 ~#2.6 학습 완료 커밋

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-alert("hi")
+const player = {
+  name: "sewol",
+  points: 10,
+  fat: true,
+};
+
+console.log(player);
+console.log(player.name);
+console.log(player["name"]);
+
+player.fat = false;
+console.log(player);
+
+player.lastName = "kim";
+console.log(player);
