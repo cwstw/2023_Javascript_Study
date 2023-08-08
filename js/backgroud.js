a48bd2ca60957055cdd79691fc6beec00cb17467
cwstw <cwstw@naver.com>
2023-08-08 23:10:07 +0900

    2023-08-08 ~#8.2 학습 완료

--- js/backgroud.js
+++ js/backgroud.js
@@ -1,8 +1,13 @@
-const images = ["01.jpeg", "02.jpg", "03.jpg"];
+let images = [];
+for (i = 0; i < 5; i++) {
+  images.push(`0${i}.jpg`);
+}
 
 const randomImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
+
+bgImage.classList.add("img-fluid");
 bgImage.src = `img/${randomImage}`;
 
 document.body.appendChild(bgImage);
